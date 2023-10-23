6fe637063fa802d3eb2ffba0679e0fb00e47775e
flokapi <flokapi@pm.me>
2023-10-23 16:38:15 +0300

    Improved the callback functions

--- apps/counter/gui/counter.py
+++ apps/counter/gui/counter.py
@@ -8,15 +8,15 @@ class Counter(ft.UserControl):
         super().__init__()
         self.api = api
 
-    async def minus(self):
+    async def minus(self, *args):
         self.txt_number.value = str(int(self.txt_number.value) - 1)
         await self.update_async()
 
-    async def plus(self):
+    async def plus(self, *args):
         self.txt_number.value = str(int(self.txt_number.value) + 1)
         await self.update_async()
 
-    async def publish(self):
+    async def publish(self, *args):
         await self.api['set_value'](int(self.txt_number.value))
 
     async def loop(self):
@@ -25,30 +25,21 @@ class Counter(ft.UserControl):
             await asyncio.sleep(5)
 
     def build(self):
-        async def minus_click(e):
-            await self.minus()
-
-        async def plus_click(e):
-            await self.plus()
-
-        async def publish_click(e):
-            await self.publish()
-
         self.txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
         
         return ft.Column(
             [
                 ft.Row(
                     [
-                        ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                        ft.IconButton(ft.icons.REMOVE, on_click=self.minus),
                         self.txt_number,
-                        ft.IconButton(ft.icons.ADD, on_click=plus_click),
+                        ft.IconButton(ft.icons.ADD, on_click=self.plus),
                     ],
                     alignment=ft.MainAxisAlignment.CENTER,
                 ),
                 ft.Row(
                     [
-                        ft.ElevatedButton('Publish', on_click=publish_click)
+                        ft.ElevatedButton('Publish', on_click=self.publish)
                     ],
                     alignment=ft.MainAxisAlignment.CENTER,
                 ),
